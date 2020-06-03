1a297bd51ac2d1a3dfdd763871ecc7e82917afd8
hippi345 <hippi345@live.com>
2020-06-03 17:04:39 -0400

    Update knapsack.py

--- python/dynamic_programming/knapsack.py
+++ python/dynamic_programming/knapsack.py
@@ -1,25 +1,42 @@
-values = [1,2,3,4]
-weights = [1,2,3,4]
-capacity = 5
-n = len(values)
+"""
+When looking at knapsack with reconstruction, the key ideas are the following:
+Solve for the optimal value the same way.
+Keep track of whether you choose an item or not in each sub-problem.
+In the end loop over the the items chosen on the overall problem.
+The ones chosen for that final sub-problem will be having values of True for the items.
+Key Tip: know the scenario for which you are adding an item vs. taking the previous best.
+The condition for making the decision value true is the same as the condition for adding the value
+of the current sub-problem item's value to the global solution.
+"""
 
-# setting all the elements to zero
-K = [[0 for x in range(capacity + 1)] for x in range(n + 1)]
-for i in range(n + 1):
-    for j in range(capacity + 1):
-        if i == 0 or j == 0:
-            K[i][j] = 0
-        # compare the weights
-        # if less than the capacity then take the max of the previous value + the difference value or the previous value
-        elif weights[i - 1] <= j:
-            K[i][j] = max(values[i - 1] + K[i - 1][j - weights[i - 1]], K[i - 1][j])
+
+def knapsack_DP_reconstruct(W, weights, values):
+    dp = [[0 for i in range(0, len(weights) + 1)] for j in range(0, W + 1)]
+    n = len(weights)
+    decisions = [[False for i in range(0, len(weights) + 1)] for j in range(0, W + 1)]
+    dp[W][0] = 0
+    for i in range(1, n + 1):
+        for w in range(0, W + 1):
+            if weights[i - 1] <= w:
+                if dp[w - weights[i - 1]][i - 1] + values[i - 1] > dp[w][i - 1]:
+                    # We record the decision here that its beneficial to pick the ith item
+                    decisions[w][i] = True
+                    dp[w][i] = dp[w - weights[i - 1]][i - 1] + values[i - 1]
+                else:
+                    dp[w][i] = dp[w][i - 1]
+            else:
+                dp[w][i] = dp[w][i - 1]
+
+    i = n
+    w = W
+    while i > 0 and w > 0:
+        if decisions[w][i]:
+            print("Picked up {} , Weight {} , Value {}".format(i - 1, weights[i - 1], values[i - 1]))
+            w -= weights[i - 1]
+            i -= 1;
         else:
-            K[i][j] = K[i - 1][j]
-print(K[n][capacity])
+            i -= 1
+    return dp[W][n]
 
-'''
-The reflection is that we compare the current subproblem weight and capacity being observed.
-If the current weight is less than the current capacity then we can set the value in our DP table to be the max of the previous row value
-and the current value plus the value differenced off the current capacity in the row above.
-The max of these two values is the current max value achievable.
-'''
+
+print(knapsack_DP_reconstruct(5, [1, 2, 3, 4], [1, 2, 3, 4]))
