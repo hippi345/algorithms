d3ffee400ecd04f8c80d23896dbbbb3861707367
hippi345 <hippi345@live.com>
2020-06-04 22:39:49 -0400

    Update lcs.py

--- python/dynamic_programming/lcs.py
+++ python/dynamic_programming/lcs.py
@@ -1,15 +1,15 @@
-str1 = "abxc";
-str2 = "abcd";
+str1 = "abxc"
+str2 = "abcd"
 n = len(str1)
 m = len(str2)
-toReturn = 0;
-DP = [[0 for k in range(n+1)] for l in range(m+1)]
+toReturn = 0
+DP = [[0 for k in range(n + 1)] for l in range(m + 1)]
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if (str1[i-1] == str2[j-1]):
-            DP[i][j] = DP[i-1][j-1] + 1
-            toReturn= max(toReturn, DP[i][j])
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if str1[i - 1] == str2[j - 1]:
+            DP[i][j] = DP[i - 1][j - 1] + 1
+            toReturn = max(toReturn, DP[i][j])
         else:
             DP[i][j] = 0
 print(toReturn)
