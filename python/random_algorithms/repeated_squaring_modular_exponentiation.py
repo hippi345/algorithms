00752856e064a4b1ee96536d73c3dac947eacf85
hippi345 <hippi345@live.com>
2020-06-07 13:26:54 -0400

    adding the fast exponentiation and modular exponentiation method

--- python/random_algorithms/repeated_squaring_modular_exponentiation.py
+++ python/random_algorithms/repeated_squaring_modular_exponentiation.py
@@ -1,4 +1,5 @@
 import math
+import datetime
 
 
 def mod_exp(x, y, N):
@@ -11,4 +12,22 @@ def mod_exp(x, y, N):
         return x * math.pow(z, 2) % N
 
 
+start_time1 = datetime.datetime.now()
+# 2 ^ 4 = 16. 16mod5 equals 1
 print(mod_exp(2, 4, 5))
+stop_time1 = datetime.datetime.now()
+time_diff1 = stop_time1 - start_time1
+print(time_diff1.total_seconds())
+
+start_time2 = datetime.datetime.now()
+# this also equals 1 but is much larger
+print(mod_exp(13, 27, 4))
+stop_time2 = datetime.datetime.now()
+time_diff2 = stop_time2 - start_time2
+print(time_diff2.total_seconds())
+
+start_time3 = datetime.datetime.now()
+print(mod_exp(13, 45, 67))
+stop_time3 = datetime.datetime.now()
+time_diff3 = stop_time2 - start_time2
+print(time_diff3.total_seconds())
